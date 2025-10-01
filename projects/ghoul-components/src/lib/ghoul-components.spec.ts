ba4a44a8d40f3f6369d2a77683b0104022bc4a15
4uruanna <adrien@villalonga.software>
2025-10-01 18:20:23 +0200

    style: lint

--- projects/ghoul-components/src/lib/ghoul-components.spec.ts
+++ projects/ghoul-components/src/lib/ghoul-components.spec.ts
@@ -3,20 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GhoulComponents } from './ghoul-components';
 
 describe('GhoulComponents', () => {
-  let component: GhoulComponents;
-  let fixture: ComponentFixture<GhoulComponents>;
+    let component: GhoulComponents;
+    let fixture: ComponentFixture<GhoulComponents>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GhoulComponents],
-    }).compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [GhoulComponents],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(GhoulComponents);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(GhoulComponents);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- projects/ghoul-components/src/lib/ghoul-components.ts
+++ projects/ghoul-components/src/lib/ghoul-components.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'gc-ghoul-components',
-  imports: [],
-  template: ` <p>ghoul-components works!</p> `,
-  styles: ``,
+    selector: 'gc-ghoul-components',
+    imports: [],
+    template: ` <p>ghoul-components works!</p> `,
+    styles: ``,
 })
 export class GhoulComponents {}
