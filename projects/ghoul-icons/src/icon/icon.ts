bcc641a2c97dec368aadc199db4830dca0d17112
4uruanna <adrien@villalonga.software>
2025-10-01 18:04:57 +0200

    feat: add ghoul icon

--- projects/ghoul-icons/src/icon/icon.ts
+++ projects/ghoul-icons/src/icon/icon.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'gc-icon',
-  imports: [],
-  templateUrl: './icon.html',
-  styleUrl: './icon.css'
+    selector: 'gc-icon',
+    imports: [],
+    templateUrl: './icon.html',
+    styleUrl: './icon.scss',
 })
 export class Icon {
+    @Input({ required: true })
+    name!: 'ghoul';
 
+    @Input()
+    size: string | number = 24;
 }
