421341574d716da6fe84039381bed79f01825c4d
4uruanna <adrien@villalonga.software>
2025-10-01 17:24:17 +0200

    chore: remove sample component

--- projects/ghoul-icons/src/lib/ghoul-icons.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'gc-ghoul-icons',
-  imports: [],
-  template: ` <p>ghoul-icons works!</p> `,
-  styles: ``,
-})
-export class GhoulIcons {}
